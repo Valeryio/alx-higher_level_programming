61cf342fb5af03110e5dfeedcbace1737163571e
Valeryio <tommagoma23@gmail.com>
2024-03-11 14:34:49 +0000

    Update the task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:66]} {str[-22:-18]} {str[:6]}")
+str1 = str[39:66] + ' ' + str[-22:-18] + ' ' + str[:6];
+print(f"{str1}")
