98e85f14dfd1f5d3daf951c1f317f1a04abd532c
Valeryio <tommagoma23@gmail.com>
2024-03-12 14:56:11 +0000

    Update the task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,10 @@
 
 def print_last_digit(number):
 
+    try:
+        numberVerificator = int(number)
+    except ValueError:
+        return
+
     print(f"{str(number)[-1]}", end='')
     return str(number)[-1]
