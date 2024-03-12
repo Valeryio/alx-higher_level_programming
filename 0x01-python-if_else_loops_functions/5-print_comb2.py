a4d83711d8214a2191f4af49da36938246bbc562
Valeryio <tommagoma23@gmail.com>
2024-03-12 22:00:02 +0000

    Update the task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    print("{:0>2d}".format(i), end='')
+    print("{:0>2d}".format(i), end=',')
     if (i + 1 != 100):
-        print(",", end=" ")
+        print(" ")
