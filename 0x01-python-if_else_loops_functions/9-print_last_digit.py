e31b64b1af6f642ece5b7b004e403e8ae0c7be5b
Valeryio <tommagoma23@gmail.com>
2024-03-12 22:09:19 +0000

    Update the task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,5 @@
 
 def print_last_digit(number):
 
-    try:
-        print(f"{int(number)}")
         print(f"{str(number)[-1]}", end='')
         return str(number)[-1]
-    except ValueError:
-        print("Traceback (most recent call last):")
