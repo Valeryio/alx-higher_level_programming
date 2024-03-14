b1f13300048311528112d83f19169122a939b040
Valeryio <tommagoma23@gmail.com>
2024-03-14 15:33:53 +0000

    Update the task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,9 +3,9 @@
     This program prints some add of numbers
 """
 
+if __name__ == "__main__"
+    from add_0 import add
 
-from add_0 import add
-
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
