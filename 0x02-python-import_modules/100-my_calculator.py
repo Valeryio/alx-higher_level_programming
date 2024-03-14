8c6b275fb217fc26693dfda69b0f7b0557225032
Valeryio <tommagoma23@gmail.com>
2024-03-14 20:53:42 +0000

    Update the task 101

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,32 +3,33 @@
 import sys
 from calculator_1 import *
 
-if (int(len(sys.argv)) != 4):
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+if __name__ == "__main__":
 
-operator = sys.argv[2]
-x = int(sys.argv[1])
-y = int(sys.argv[3])
-operators = ['+', '-', '*', '/']
+    if (int(len(sys.argv)) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-if operator not in operators:
-    print("Unknown operator. Available operators: +, -, * and /");
-    sys.exit(1)
+    operator = sys.argv[2]
+    x = int(sys.argv[1])
+    y = int(sys.argv[3])
+    operators = ['+', '-', '*', '/']
 
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-match operator:
-    case '+':
-        print("{a} {op} {b} = {c}".format(a=x, op=operator, b=y, c=add(x, y)))
+    match operator:
+        case '+':
+            print("{} {} {} = {}".format(x, operator, y, add(x, y)))
 
-    case '-':
-        print("{a} {op} {b} = {c}".format(a=x, op=operator, b=y, c=sub(x, y)))
+        case '-':
+            print("{} {} {} = {}".format(x, operator, y, sub(x, y)))
 
-    case '*':
-        print("{a} {op} {b} = {c}".format(a=x, op=operator, b=y, c=mul(x, y)))
+        case '*':
+            print("{} {} {} = {}".format(x, operator, y, mul(x, y)))
 
-    case '/':
-        print("{a} {op} {b} = {c}".format(a=x, op=operator, b=y, c=div(x, y)))
+        case '/':
+            print("{} {} {} = {}".format(x, operator, y, div(x, y)))
 
-    case _:
-        pass
+        case _:
+            pass
