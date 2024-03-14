9f7c29699f0f1d05060cd7a5d5184661f9dc684e
Valeryio <tommagoma23@gmail.com>
2024-03-14 19:51:30 +0000

    Update the task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,4 +9,3 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, calc.add(a, b)))
     print("{} - {} = {}".format(a, b, calc.sub(a, b)))
     print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calc.div(a, b)))
