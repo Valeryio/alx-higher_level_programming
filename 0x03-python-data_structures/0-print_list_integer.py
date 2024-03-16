73cc936a6782c2aaaa3ba0deb36b8081e3aafe94
Valeryio <tommagoma23@gmail.com>
2024-03-16 09:35:22 +0000

    Update the task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
+    """My print function
 
-    for i in my_list:
-        print("{}".format(i))
+    Args:
+        my_list: a list
+    """
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
