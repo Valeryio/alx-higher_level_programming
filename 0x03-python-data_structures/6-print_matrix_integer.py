94e8693d3f5e5e13f8121dac76e67dba2bd700b5
Valeryio <tommagoma23@gmail.com>
2024-03-16 20:32:16 +0000

    Update the task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """
+        This function prints a matrix
+
+    Args:
+        matrix: matrix
+
+    Returns:
+        Returns nothing
+    """
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i]))
+            print("{}".format(matrix[i][j]), end=" ")
+        print()
