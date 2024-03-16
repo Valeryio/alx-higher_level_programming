997e6a7f1b6182f4dac6867e734dda034a896b70
Valeryio <tommagoma23@gmail.com>
2024-03-16 10:09:46 +0000

    Update the task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
     """My print function
 
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 
+def element_at(my_list, idx):
+    """
+        My search function
 
+        Args:
+            my_list: the list
+            idx: the index of the searched element
+
+        Returns:
+            Returns the value at the index
+    """
+    if (idx < 0 or idx >= len(my_list)):
+        return None
+
+    return my_list[idx]
