eb3d625befdcf155af4f62aec9bae6878c2f2cef
Valeryio <tommagoma23@gmail.com>
2024-03-16 20:35:02 +0000

    Update the task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """
-        My print function
 
-        Args:
-            my_list: a list
-    """
-    try:
-        int(my_list)
-    except TypeError:
-        pass
+    # print a list of integer
 
     for i in my_list:
-        print("{a}".format(a=i))
+        print("{}".format(i))
