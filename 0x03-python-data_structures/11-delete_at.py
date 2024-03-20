e1abe183c99214944770cecd486e17c7017928cd
Valeryio <tommagoma23@gmail.com>
2024-03-20 02:57:34 +0000

    Update the task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -11,15 +11,8 @@ def delete_at(my_list=[], idx=0):
     Returns:
         A new list
     """
-
     new_list = []
-    to_delete = 0
-
-    for i in my_list:
-        if i == my_list[idx]:
-            to_delete = i
-
-    my_list.remove(to_delete)
+    my_list.pop(idx)
     new_list = my_list
 
     return new_list
