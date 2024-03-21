d62133a3fd139bf345f3469eeb37cd5f968b0ae6
Valeryio <tommagoma23@gmail.com>
2024-03-21 10:11:19 +0000

    Update the task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+        This function analyse a dict
+    Args:
+        @a_dictionary: a dict
+    Returns:
+        An integer
+    """
+    return (len(a_dictionary))
