b06f3c29e93df17bc48228e9e02cbaf57b4cc74c
Valeryio <tommagoma23@gmail.com>
2024-03-21 09:39:00 +0000

    Update the task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+def uniq_add(my_list = []):
+    """
+        This function adds unique integers
+        in a list
+    Args:
+        my_list: the list of integes
+    Result:
+        An integer
+    """
+
+    new_list = []
+
+    for i in range(len(my_list)):
+        if my_list[i] in new_list:
+            continue
+        else:
+            new_list.append(my_list[i])
+
+    return sum(new_list)
