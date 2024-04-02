e9c3072b02f5d6688c0f033fdb0722ae0db745ad
Valeryio <tommagoma23@gmail.com>
2024-04-02 11:47:48 +0000

    Update the task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -8,6 +8,6 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(int(value)))
-        return True
-    except ValueError :
+    except:
         return False
+    return True
