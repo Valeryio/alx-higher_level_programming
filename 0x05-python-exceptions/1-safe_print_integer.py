2bbc6ade5f80a02dfd4783ffb890d34a70be3bb6
Valeryio <tommagoma23@gmail.com>
2024-04-02 09:48:10 +0000

    Update the task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    """
+        print safely an integer
+    Args:
+        @value: A value to print
+    Return: A boolean
+    """
     try:
         print("{:d}".format(int(value)))
         return True
