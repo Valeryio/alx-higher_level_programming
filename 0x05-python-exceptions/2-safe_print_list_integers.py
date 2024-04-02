23dc30eb3420efceb4f31a6428f8cfa73e5e4dd7
Valeryio <tommagoma23@gmail.com>
2024-04-02 10:45:31 +0000

    Update the task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """
+        Print safely a list
+
+    Args:
+        @mylist: The list to print
+        @x: the number to use
+    Return: An integer
+    """
+    j = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(int(my_list[i])), end="")
+            j += 1
+        except (ValueError, TypeError):
+            pass
+
+    print("")
+    return j
