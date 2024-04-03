a38ac4571c63e66999cda5fd0fd842b6c2149cb5
Valeryio <tommagoma23@gmail.com>
2024-04-03 14:02:54 +0000

    Update the task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This is a module for a square
+"""
+
+
+class Square:
+    """This is an empty class of square
+
+    Note:
+    Args:
+    Attributes:
+    """
+    def __init__(self, size=0):
+        self.__size = size
