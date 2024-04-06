eaa0ca7719e528312b29dc2c543e3806982eefc0
Valeryio <tommagoma23@gmail.com>
2024-04-06 13:48:28 +0000

    Writing the different test
    All test pass with the actual code. Let's try to make it more clear and readable!

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    This module contains the implementation of the sum function.
+"""
+def add_integer(a, b=98):
+    """This function returns the sum of two integers
+        
+        :param a: an integer or a float
+        :param b: an integer of a float
+
+        :return: an integer
+    """
+    if isinstance(a, (list, dict, tuple)):
+        raise TypeError("a must be an integer")
+
+    if isinstance(b, (list, dict, tuple)):
+        raise TypeError("b must be an integer")
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/doctestfile.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# document test file
+
+import doctest
+
+if __name__ == '__main__':
+    doctest.testfile('./test/0-add_integer.txt')
