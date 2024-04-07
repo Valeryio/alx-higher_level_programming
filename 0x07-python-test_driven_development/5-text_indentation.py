de7ab7f8515ab9f03db68350284486587c43c91a
Valeryio <tommagoma23@gmail.com>
2024-04-07 18:10:56 +0000

    Starting the task 4
    Lets implement a function that will print a text with some verifications

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module has the only one function text indentation
+    :param text: A text
+"""
+
+def text_indentation(text):
+    """
+        This function returns prints a text with new line characters
+        :return nothing
+    """
