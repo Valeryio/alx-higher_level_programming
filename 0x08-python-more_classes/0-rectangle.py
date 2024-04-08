dd1e4bfecfc1872215e4b8605fb0f4e0d181ca08
Valeryio <tommagoma23@gmail.com>
2024-04-08 12:41:34 +0000

    End of the task 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This module is about a class that we created to manipulate rectangles
+"""
+    
+    class Rectangle:
+    """This is an empty class of rectangle
+
+        Note:
+        Args:
+        Attributes:
+    """
+    
+        def __init__(self):
+            pass
