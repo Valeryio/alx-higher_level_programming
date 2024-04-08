3deaa6636c836a5423e4e81f17489961afd8ec7c
Valeryio <tommagoma23@gmail.com>
2024-04-08 14:08:46 +0000

    First version of the task 2
    All the test pass! Everything is green for me. Next to the checker! Pycodestyle
    also pass

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+    This module is about a class that we created to manipulate rectangles
+"""
+
+
+class Rectangle:
+    """This is an empty class of rectangle
+
+        Note:
+        Args:
+        Attributes:
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def perimeter(self):
+        return (self.__width + self.__height) * 2
+
+    def area(self):
+        return self.__width * self.__height
