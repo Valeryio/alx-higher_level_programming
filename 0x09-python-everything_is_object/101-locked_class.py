04d72e3f2261490d8a82967d098f3c48ba9f760a
Valeryio <tommagoma23@gmail.com>
2024-04-09 12:40:57 +0000

    commit the class 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""THis module contains a locked class that don't
+    allow user to add dynamically attributes to the class
+"""
+
 
 class LockedClass:
+    """This is a locked class"""
     __slots__ = ('first_name')
