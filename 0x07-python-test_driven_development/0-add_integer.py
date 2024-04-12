c804f523e249eea8301b46094146c8186d179e4f
Valeryio <tommagoma23@gmail.com>
2024-04-12 20:59:06 +0000

    UPdating code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,9 +14,9 @@ def add_integer(a, b=98):
     if isinstance(b, (list, dict, tuple)):
         raise TypeError("b must be an integer")
 
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or a == inf or a == -inf:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)) or b == inf or b == -inf:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
