269e7d454995da9703e797f474821b7d630883c5
Valeryio <tommagoma23@gmail.com>
2024-04-16 14:37:53 +0000

    Updating the task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,21 +4,22 @@
 """
 import json
 import sys
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
+if not os.path.exists('add_item.json'):
+    # Write directly for the first time
+    save_to_json_file(sys.argv[1:], "add_item.json")
 
-# Open the file with the appending
-with open("add_item.json", 'a', encoding="UTF-8") as json_file:
-    data = load_from_json_file(add)
+else:
+    # Upload the data as python object from the file
+    data = load_from_json_file("add_item.json")
 
+    # Appending new object to the object
     for i in sys.argv[1:]:
         data.append(i)
 
-
-
-#    save_to_json_file(sys.argv[1:], "myfile.txt")
-# Deserialize from this tmp file
-
-#   json.dump(data, json_file)
+    # Write the object as json in the json's file
+    save_to_json_file(data, "add_item.json")
