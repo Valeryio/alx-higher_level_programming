c4b42594d992ffbc40a58d52042e3d3f9c715f41
Valeryio <tommagoma23@gmail.com>
2024-04-17 11:45:45 +0000

    Commit on the task 8

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('6-base_geometry').BaseGeometry
+Rectangle = __import__('8-rectangle').Rectangle
 
-bg = BaseGeometry()
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
 
 try:
-    print(bg.area())
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
