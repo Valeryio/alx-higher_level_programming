461340cc3f30e18ad5fe138cf5f23d17ba0df676
Valeryio <tommagoma23@gmail.com>
2024-04-17 08:26:43 +0000

    Update the task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""
+    This module contains a function that list attributes and methods of
+    classes
+"""
 
+
+def lookup(obj):
+    """
+        This function returns all the methods and the attributes of a
+        class object
+    Args:
+        obj (class): An instance of an user-defined class
+    Returns:
+        A list
+    """
+    return obj.__dict__
