c5bd42c0f28ba76e167beb76e646d4e214f55b8b
Valeryio <tommagoma23@gmail.com>
2024-04-17 11:53:16 +0000

    commit on the task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -15,7 +15,7 @@ class BaseGeometry():
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not type(value) == int:
+        if not type(value) is int:
             raise TypeError(f"{name} must be an integer")
 
         if value <= 0:
