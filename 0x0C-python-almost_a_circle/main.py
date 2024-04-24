88e6990004d804cd464bffb28471de5639c7a2ab
Valeryio <tommagoma23@gmail.com>
2024-04-24 08:55:22 +0000

    Updating the task 15

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -2,14 +2,16 @@
 """ Check """
 from models.base import Base
 
-rjson = Base.to_json_string([])
+list_dictionaries = []
+rjson = Base.to_json_string(list_dictionaries)
+rjson_expected = "[]"
 
 if rjson is None:
     print("to_json_string is not returning a string")
     exit(1)
 
-if type(rjson) is not str:
-    print("to_json_string is not returning a string: {}".format(type(rjson)))
+if rjson != rjson_expected:
+    print("to_json_string on {} must return {}: {}".format(list_dictionaries, rjson_expected, rjson))
     exit(1)
 
 print("OK", end="")
