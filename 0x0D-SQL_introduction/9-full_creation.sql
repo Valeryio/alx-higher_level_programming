60fc2cc8f5edfe3d2abf7f0827944f3f5a2a736c
Valeryio <tommagoma23@gmail.com>
2024-05-21 14:11:23 +0000

    update on the task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,13 @@
 -- full table creation
 
-USE hbtn_0c_0;
 CREATE TABLE second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
-)
+);
 
 INSERT INTO second_table(id, name, score) VALUES
 (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
-(4, "George", 8)
+(4, "George", 8);
