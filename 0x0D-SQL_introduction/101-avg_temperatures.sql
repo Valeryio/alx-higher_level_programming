128554889245dd25712effd60ade58d02c6f2830
Valeryio <tommagoma23@gmail.com>
2024-05-21 15:37:23 +0000

    update the task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,8 +1,8 @@
 -- import and display temperature
 
-SOURCE temperatures.sql
+SOURCE ./temperatures.sql;
 
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY avg_temp DESC
+ORDER BY avg_temp DESC;
