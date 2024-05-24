c88c4e57ff6d9eed92f80d4898a6b09c3c9d644b
Valeryio <tommagoma23@gmail.com>
2024-05-24 13:41:50 +0000

    update the task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,4 @@ FROM tv_show_genres
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 GROUP BY genre
-ORDER BY number_of_shows;
+ORDER BY number_of_shows DESC;
