7150957fe3a312bbafc9df280d0634f45c8c2f7a
Valeryio <tommagoma23@gmail.com>
2024-05-24 14:27:47 +0000

    update the task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,5 +6,5 @@ SELECT tv_genres.name
 FROM tv_show_genres
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title != "Dexter")
+WHERE tv_shows.title = "Dexter")
 ORDER BY name ASC;
