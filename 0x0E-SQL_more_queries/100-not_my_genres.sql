7e65433029a91412d73dfcbf756663cb6d8afb59
Valeryio <tommagoma23@gmail.com>
2024-05-24 14:27:07 +0000

    commit on the task 100

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- not my genres
+SELECT *
+FROM tv_genres
+WHERE name NOT IN (
+SELECT tv_genres.name
+FROM tv_show_genres
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title != "Dexter")
+ORDER BY name ASC;
