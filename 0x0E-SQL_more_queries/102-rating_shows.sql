d11618729af4195b7aa157719abc86f8c1353dff
Valeryio <tommagoma23@gmail.com>
2024-05-24 14:46:11 +0000

    update the task 102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,2 +1,4 @@
 -- rating shows:x
 
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings INNER JOIN tv_shows ON tv_show_ratings.show_id = tv_shows.id GROUP BY title ORDER BY rating DESC;
+
