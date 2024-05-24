d55ffa638775141a4c6dc5e394f9122772016c03
Valeryio <tommagoma23@gmail.com>
2024-05-24 13:14:47 +0000

    commit the task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- left join
+
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY title, genre_id;
