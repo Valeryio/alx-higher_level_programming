a6ad4a274af5fd0b4256ca940786948a808f5574
Valeryio <tommagoma23@gmail.com>
2024-05-24 13:19:15 +0000

    update the task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,5 +4,5 @@ SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE genre_id = NULL
+WHERE genre_id IS NULL
 ORDER BY title, genre_id;
