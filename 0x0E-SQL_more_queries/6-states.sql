989c6200f51628f46d02cdfb7fe02051536edda3
Valeryio <tommagoma23@gmail.com>
2024-05-24 04:37:42 +0000

    commit on the task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create states databases
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS state(
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	name VARCHAR(256));
