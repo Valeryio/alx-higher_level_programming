bb0138c424275740289cab40fad5a585d6e1ed3f
Valeryio <tommagoma23@gmail.com>
2024-05-24 13:40:38 +0000

    update the task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- count show by genre
 
-SELECT tv_show_genres.name AS genre, COUNT(tv_shows_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_show_genres
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
