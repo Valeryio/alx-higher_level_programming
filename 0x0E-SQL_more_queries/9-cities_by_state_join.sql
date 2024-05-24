e7ed30aecbe26d06dc2dc7eb5864a5d4e929538b
Valeryio <tommagoma23@gmail.com>
2024-05-24 11:25:04 +0000

    update on the task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- join operation
 
-SELECT cities.id as id, cities.name as name, states.name as name
+SELECT cities.id AS id, cities.name AS name, states.name AS name
 FROM cities INNER JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id;
