464fe03583612049f4bd3320a90d9da09d4cb957
Valeryio <tommagoma23@gmail.com>
2024-05-24 14:10:13 +0000

    update on the task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_show_genres
 RIGHT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-ORDER BY title, genre_id;
+ORDER BY title, name;
