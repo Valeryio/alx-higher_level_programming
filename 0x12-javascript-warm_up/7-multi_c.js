775f86d65982f55eac0f927ae1c61ffb56a910df
Valeryio <tommagoma23@gmail.com>
2024-06-18 13:08:04 +0000

    commit on the task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+let x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurences');
+}  else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
