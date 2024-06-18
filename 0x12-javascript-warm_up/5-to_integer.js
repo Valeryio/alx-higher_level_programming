045e2412219c983bc756c10ad689e1194d9b379c
Valeryio <tommagoma23@gmail.com>
2024-06-18 12:53:07 +0000

    commit on the task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+let myNumber;
+
+myNumber = parseInt(process.argv[2]);
+
+if (isNaN(myNumber)) {
+  console.log('Not a number');
+} else {
+  console.log(myNumber);
+}
