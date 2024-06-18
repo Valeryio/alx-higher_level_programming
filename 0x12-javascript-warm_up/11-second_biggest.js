182f5574738861e88b5e35d6f8406a4e58f5e3f1
Valeryio <tommagoma23@gmail.com>
2024-06-18 15:15:25 +0000

    update on the task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const process = require('process');
-const newArray = process.argv.slice(2, process.argv.length);
+const newArray = process.argv.slice(2);
 
 let tmp;
 
@@ -11,12 +11,13 @@ if (process.argv.length < 3) {
   console.log(0);
 } else {
   for (let i = 0; i < newArray.length; i++) {
-    for (let j = 0; j < newArray.length - i; j++)
+    for (let j = 0; j < newArray.length - i; j++) {
       if (newArray[j] < newArray[j + 1]) {
         tmp = newArray[j];
 	newArray[j] = newArray[j + 1];
 	newArray[j + 1] = tmp;
       }
+    }
   }
   console.log(newArray[1]);
 }
