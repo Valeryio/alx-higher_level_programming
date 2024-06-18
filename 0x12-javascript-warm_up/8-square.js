94367e34256404928686311b01426e8658f0aeeb
Valeryio <tommagoma23@gmail.com>
2024-06-18 13:40:24 +0000

    update on the task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      process.stdout.write('X');
+   }
+  console.log('');
+  }
+}
