7363240c337b226ae5bd1122312a95710ed6d44a
Valeryio <tommagoma23@gmail.com>
2024-06-18 15:31:09 +0000

    commit on the task 11

--- 0x12-javascript-warm_up/test_11.js
+++ 0x12-javascript-warm_up/test_11.js
@@ -11,14 +11,13 @@ if (process.argv.length < 3) {
   console.log(0);
 } else {
   for (let i = 0; i < newArray.length; i++) {
-    for (let j = 0; j < newArray.length - 1 - i; j++) { // Fixed the loop condition
+    for (let j = 0; j < newArray.length - i; j++) {
       if (newArray[j] < newArray[j + 1]) {
         tmp = newArray[j];
-        newArray[j] = newArray[j + 1];
-        newArray[j + 1] = tmp;
+	newArray[j] = newArray[j + 1];
+	newArray[j + 1] = tmp;
       }
     }
   }
   console.log(newArray[1]);
 }
-
