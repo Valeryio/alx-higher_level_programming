37220dd71192d44e52530ab2351c719eafb6e68c
Valeryio <tommagoma23@gmail.com>
2024-06-18 12:18:06 +0000

    update on the task 4

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,10 +4,10 @@ const process = require('process');
 let i = 2;
 
 if (process.argv.length < 3) {
-  console.log("No argument");
+  console.log('No argument');
 } else {
-  while (process.argv[i] != undefined) {
-  console.log(process.argv[i]);
-  i++;
+  while (process.argv[i] !== undefined) {
+    console.log(process.argv[i]);
+    i++;
   }
 }
