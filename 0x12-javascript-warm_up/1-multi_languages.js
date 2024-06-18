949fde62fd2cc627ebc23c5abaa68fa0a9739fef
Valeryio <tommagoma23@gmail.com>
2024-06-18 11:47:58 +0000

    new update on the task 4

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let info = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let info = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < info.length; i++)
 {
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-/**
- * This the task 5
- */
-// This is a comment for the task 5
 
-const process = require("process");
+const process = require('process');
 
-console.log(process.argv[2], "is", process.argv[3])
+console.log(process.argv[2], 'is', process.argv[3])
