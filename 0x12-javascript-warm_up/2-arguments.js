6e0b97973933d1fb269e988a759da0474d05af4d
Valeryio <tommagoma23@gmail.com>
2024-06-18 12:08:46 +0000

    update on the task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const process = require('process');
 
 if (process.argv.length < 3) {
   console.log('No argument');
+} else if (process.argv.length == 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
