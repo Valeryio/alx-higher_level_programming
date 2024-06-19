0e0382a97cbb91386d54d280092bf1699ffa56b4
Valeryio <tommagoma23@gmail.com>
2024-06-19 10:24:56 +0000

    update on the task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,18 @@
 
 const process = require('process');
 
-const newArray = process.argv.slice(2); let tmp;
+const newArray = process.argv.slice(2);
+let tmp;
+
+for (let i = 0; i < newArray.length; i++) {
+  newArray[i] = parseInt(newArray[i]);
+}
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
   for (let i = 0; i < newArray.length; i++) {
-    for (let j = 0; j < newArray.length - 1 - i; j++) { // Fixed the loop condition
+    for (let j = 0; j < newArray.length - i; j++) { // Fixed the loop condition
       if (newArray[j] < newArray[j + 1]) {
         tmp = newArray[j];
         newArray[j] = newArray[j + 1];
