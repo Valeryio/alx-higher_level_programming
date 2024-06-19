bf8e9c72f61861829da23a7a87ab75b2950fc748
Valeryio <tommagoma23@gmail.com>
2024-06-19 14:43:52 +0000

    commit on the task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,9 +3,17 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-
   constructor (size) {
     super(size, size);
+  }
+
+  charPrint (symbol = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        process.stdout.write(symbol);
+      }
+      console.log('');
+    }
   }
 }
 
