e2c18250c19fa9f01c836374f9345fea90b5d91c
Valeryio <tommagoma23@gmail.com>
2024-06-23 22:22:24 +0000

    night update on the task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# if __name__ == "__main__":
+
 import os
 import sys
 import MySQLdb
@@ -7,32 +9,60 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+    
 
 os.environ['SQLALCHEMY_SILENCE_UBER_WARNING'] = '1'
 
-user = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+def start_engine():
+    """This function creates a new engine object
+    Returns:
+        An engine object
+    """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-engine = create_engine(f"mysql+pymysql://{user}:{password}@localhost:3306/{database}")
-Base = declarative_base()
+    return create_engine(f"mysql+pymysql://{user}:\
+{password}@localhost:3306/{database}")
 
 
-class States(Base):
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+if __name__ == "__main__":
 
-    def __repr__(self):
-        return f"({self.id}, '{self.name}')"
+    """
+    try:
+        engine = start_engine
+    except Exception as ex:
+        print(f"The connection to the database have failed!")
+    """
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = start_engine()
+    # Instanciation of the Base class that will extends the defined
+    # mapped objects
+    Base = declarative_base()
 
-all_states = session.query(States).all()
+    class States(Base):
+        """This is the mapped States class that have a relation to the
+            simple states table in the database
+        Attributes:
+            @__tablename__: (class attribute)
+            @id: (int), the id of the state
+            @name: (str), the name of the state
+        """
+        __tablename__ = "states"
+        id = Column(Integer, primary_key=True)
+        name = Column(String)
 
-for state in all_states:
-    print(state)
+        def __repr__(self):
+            return f"({self.id}, '{self.name}')"
+
+    # Instanciating the Session object with the right database engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    all_states = session.query(States).all()
+
+    for state in all_states:
+        print(state)
 #    print("(", state.id, ", ", state.name, ")")
 
-session.close()
+    session.close()
