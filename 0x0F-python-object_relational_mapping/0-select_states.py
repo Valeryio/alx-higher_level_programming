6e5e68006747a4c27cdd7b849d097ed5e00eb29e
Valeryio <tommagoma23@gmail.com>
2024-06-23 15:11:25 +0000

    update on the task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import MySQLdb
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
@@ -9,7 +10,11 @@ from sqlalchemy import Column, Integer, String
 
 os.environ['SQLALCHEMY_SILENCE_UBER_WARNING'] = '1'
 
-engine = create_engine("mysql+pymysql://root:Val_Ery28!@localhost:3306/hbtn_0e_0_usa")
+user = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+engine = create_engine(f"mysql+pymysql://{user}:{password}@localhost:3306/{database}")
 Base = declarative_base()
 
 
@@ -18,12 +23,16 @@ class States(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
+    def __repr__(self):
+        return f"({self.id}, '{self.name}')"
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
 all_states = session.query(States).all()
 
 for state in all_states:
-    print(state.id, " : ", state.name)
+    print(state)
+#    print("(", state.id, ", ", state.name, ")")
 
 session.close()
