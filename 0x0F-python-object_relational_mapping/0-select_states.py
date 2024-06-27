3964bd77763a49e6c3eda0c89664d6aac930cc03
Valeryio <tommagoma23@gmail.com>
2024-06-27 12:55:42 +0000

    New update with simple code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa
+""" This code list all the states from a given database
 """
 
 import sys
@@ -9,4 +9,5 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states`")
-    [print(state) for state in cur.fetchall()]
+    for state in cur.fetchall():
+        print(state)
