09afde545b4a6f90a5b16b1e9a710e83c1520a4b
Valeryio <tommagoma23@gmail.com>
2024-06-28 13:58:59 +0000

    Another update of imported modules for the task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
