b5d679baf842e26d47210a5a29b0b3e603647d92
Valeryio <tommagoma23@gmail.com>
2024-06-28 14:14:04 +0000

    new update model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,52 +3,28 @@
 """ This is the module for instanciate a simple connection with sqlalchemy
 """
 
-import os
-import sys
-import sqlalchemy
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-if __name__ = "__main__"
 
-    def start_engine():
-        """This function creates a new engine object
-        Returns:
-            An engine object
-        """
-        user = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-
-        return create_engine(f"mysql+pymysql://{user}:\
-                            {password}@localhost:3306/{database}")
+"""Instanciation of the Base class that will extends the defined
+mapped objects"""
+Base = declarative_base()
 
 
-    """ Create the engine that will communicate with the SQL Database"""
-    engine = start_engine()
+class State(Base):
+    """This is the mapped States class that have a relation to the
+        simple states table in the database
+    Attributes:
+        @__tablename__: (class attribute)
+        @id: (int), the id of the state
+        @name: (str), the name of the state
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
 
-    """Instanciation of the Base class that will extends the defined
-    mapped objects"""
-    Base = declarative_base()
-
-
-    class State(Base):
-        """This is the mapped States class that have a relation to the
-            simple states table in the database
-        Attributes:
-            @__tablename__: (class attribute)
-            @id: (int), the id of the state
-            @name: (str), the name of the state
-        """
-        __tablename__ = "states"
-        id = Column(Integer, primary_key=True, autoincrement=True,
-                    nullable=False)
-        name = Column(String(128), nullable=False)
-
-        def __repr__(self):
-            """Sets the internal representation of the class"""
-            return f"({self.id}, '{self.name}')"
-
-    # Create new tables in the specified database
-    # Base.metadata.create_all(engine)
+    def __repr__(self):
+        """Sets the internal representation of the class"""
+        return f"({self.id}, '{self.name}')"
