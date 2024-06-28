4bd07094d51a94a320a4e311646a49d3b26ef139
Valeryio <tommagoma23@gmail.com>
2024-06-28 12:08:56 +0000

    commit the task 5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM cities ORDER BY 'cities.id'")
+    cur.execute("SELECT id, name FROM cities ORDER BY id")
 
     for state in cur.fetchall():
         print(state)
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This code list all the states from a given database
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM states INNER JOIN cities ON \
+                states.id=cities.state_id WHERE states.name = %s", \
+                (sys.argv[4], ))
+
+    result = cur.fetchall()
+
+    for i in range(len(result)):
+        print(result[i][0], end="")
+        if i + 1 < len(result):
+            print("", end=', ')
