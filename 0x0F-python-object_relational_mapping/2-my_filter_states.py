032e28a3331c63b4c77fa6a1dc3eac626422ca85
Valeryio <tommagoma23@gmail.com>
2024-06-28 11:03:14 +0000

    Update the task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This code list all the states from a given database
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}'".format(sys.argv[4]))
+    for state in cur.fetchall():
+        print(state)
