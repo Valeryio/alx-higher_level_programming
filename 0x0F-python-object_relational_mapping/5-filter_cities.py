5e6cc8912756a32319613c688b3413197dcdde18
Valeryio <tommagoma23@gmail.com>
2024-06-28 12:25:07 +0000

    Update on the task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ This code list all the states from a given database
 """
 
@@ -9,7 +10,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM states INNER JOIN cities ON
+    cur.execute("SELECT cities.name FROM states INNER JOIN cities ON\
                 states.id=cities.state_id WHERE states.name=%s",
                 (sys.argv[4],))
 
