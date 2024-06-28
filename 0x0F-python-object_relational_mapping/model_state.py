40c7eaf359876104c71572393841c05746b5267c
Valeryio <tommagoma23@gmail.com>
2024-06-28 12:49:12 +0000

    Update on the task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+""" This is the module for instanciate a simple connection with sqlalchemy"""
+
+import os
+import sys
+import MySQLdb
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+os.environ['SQLALCHEMY_SILENCE_UBER_WARNING'] = '1'
+
+def start_engine():
+    """This function creates a new engine object
+    Returns:
+        An engine object
+    """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    return create_engine(f"mysql+pymysql://{user}:\
+{password}@localhost:3306/{database}")
+
+
+if __name__ == "__main__":
+
+    engine = start_engine()
+    """Instanciation of the Base class that will extends the defined
+    mapped objects"""
+    Base = declarative_base()
+
+    class State(Base):
+        """This is the mapped States class that have a relation to the
+            simple states table in the database
+        Attributes:
+            @__tablename__: (class attribute)
+            @id: (int), the id of the state
+            @name: (str), the name of the state
+        """
+        __tablename__ = "states"
+        id = Column(Integer, primary_key=True)
+        name = Column(String)
+
+        def __repr__(self):
+            return f"({self.id}, '{self.name}')"
+
+    # Create new tables in the specified database
+    Base.metadata.create_all(engine)
