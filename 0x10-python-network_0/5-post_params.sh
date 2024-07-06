6289b8a2e4be061da848325d2029c81c90643f94
Valeryio <tommagoma23@gmail.com>
2024-07-06 20:03:30 +0000

    new update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script curl a page
-# curl -s --data-urlencode "email=test@gmail.com&subject=I will always be here for PLD" "$1"
 curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
