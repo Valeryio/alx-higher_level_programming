782a462e0bc93b2e139c1c0f1d50e333753cea77
Valeryio <tommagoma23@gmail.com>
2024-07-06 13:22:25 +0000

    update the task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script curl the content length on a page
-
-IP=$1
-curl -s "$IP" -I | grep Content-Length | cut -d':' -f 2 >&1
+curl -s "$1" -I | grep Content-Length | cut -d':' -f 2 >&1
