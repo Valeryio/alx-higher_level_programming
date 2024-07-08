1279d5e18b436bf6c9ad2625fc1728cdd824ff01
Valeryio <tommagoma23@gmail.com>
2024-07-08 15:55:11 +0000

    commit the code 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# This module gets the header of a requests
+
+import sys
+import urllib.request
+
+
+url = sys.argv[1]
+mail = sys.argv[2]
+
+value = {"email" : mail}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+    print(the_page)
