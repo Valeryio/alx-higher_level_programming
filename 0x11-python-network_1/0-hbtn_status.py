4aa9f85a15c037ea80eefe6f6900cb3840e22cd6
Valeryio <tommagoma23@gmail.com>
2024-07-08 14:54:01 +0000

    commit the task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# This file make a request with urllib
+
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    result = response.read()
+    print("Body response:"
+            "\n\t- type:", type(result),\
+            "\n\t- content:", result,\
+            "\n\t- utf8 content: OK")
