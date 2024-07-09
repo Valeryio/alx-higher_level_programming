bc8be67cda62333781889f5e48030568f8d1bac4
Valeryio <tommagoma23@gmail.com>
2024-07-09 10:33:19 +0000

    update the task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,4 +9,7 @@ if __name__ == "__main__":
 
     r = requests.get("https://alx-intranet.hbtn.io/status")
     text = r.content.decode("utf-8")
-    print(text)
+
+    print("Body response:")
+    print("\t- type:", type(text))
+    print("\t- content:", text)
