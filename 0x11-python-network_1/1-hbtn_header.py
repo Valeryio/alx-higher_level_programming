659e2addf757b78b225bb39c98495c05af720550
Valeryio <tommagoma23@gmail.com>
2024-07-09 08:40:28 +0000

    update the task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-"""This module gets the X-Request-Id of a header"""
+""" This module gets the X-Request-Id of a header """
 
 import sys
 import urllib.request
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    new_dict = {}
-    for key, value in response.getheaders():
-        new_dict.setdefault(key, value)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        new_dict = {}
+        for key, value in response.getheaders():
+            new_dict.setdefault(key, value)
 
-    print(new_dict["X-Request-Id"])
+        print(new_dict["X-Request-Id"])
