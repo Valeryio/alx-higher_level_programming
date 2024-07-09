444a78b882481b34661c43183178bec1a7b915ee
Valeryio <tommagoma23@gmail.com>
2024-07-09 11:26:29 +0000

    commit the task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" THis is a program to test json api of the requests module """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    try:
+        param = sys.argv[1]
+    except IndexError:
+        param = ""
+
+    value = {"q": param}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=value)
+
+    if type(r.json()) is not dict:
+        print("Not a valid JSON")
+    elif r.json() == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(r.json()['id'], r.json()['name']))
