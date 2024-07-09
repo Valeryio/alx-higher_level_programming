9d006618d4a35c29fe272eeeffbad1101df8cb9c
Valeryio <tommagoma23@gmail.com>
2024-07-09 08:49:01 +0000

    update the task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
-"""This module makes a post request"""
+""" This module makes a post request """
 
 import sys
 import urllib.request
 
 
-url = sys.argv[1]
-mail = sys.argv[2]
+if __name__ == "___main__":
+    url = sys.argv[1]
+    mail = sys.argv[2]
 
-value = {"email": mail}
-data = urllib.parse.urlencode(value)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
+    value = {"email": mail}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print(the_page)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page)
