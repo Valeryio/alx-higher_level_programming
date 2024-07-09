eddf2db79b1200807a356bc70bee9e2ac5047f39
Valeryio <tommagoma23@gmail.com>
2024-07-09 10:11:55 +0000

    commit the task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    This code send a request to an url and manage the exceptions
+    with HTTPError from the urllib library
+"""
+
+import sys
+import urllib.request
+import urllib.error
+
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        result = response.read().decode("utf-8")
+        print(result)
+except urllib.error.HTTPError as Error:
+    print("Error code:", Error.status)
