74a148a0b773caefd90b0c3de6d80172303429a7
Valeryio <tommagoma23@gmail.com>
2024-07-10 14:49:11 +0000

    commit the task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# This program use some of my credentials to get my github ID
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    token = sys.argv[2]
+#    values = {"username": sys.argv[1], "password": sys.argv[2]}
+    r = requests.get(f"https://api.github.com/users/{username}")
+
+    if r.status_code == 200:
+        print(r.json()['id'])
